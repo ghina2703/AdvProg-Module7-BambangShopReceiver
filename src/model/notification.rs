1c548277d64146ac59089fd81ccab4b2a772b36c
ghina2703 <ghina2703@gmail.com>
2025-03-28 12:46:22 +0700

    Create Notification service struct skeleton

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -15,9 +15,9 @@ pub struct Notification {
 impl Display for Notification {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         if self.status.to_uppercase().eq("CREATED") {
-            return write!(f,
+            write!(f,
                           "Hello {}, let's try our new {} product: {}, only in BambangShop! Check it out: {}",
-                          self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url);
+                          self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url)
         } else if self.status.to_uppercase().eq("DELETED") {
             return write!(f,
                           "Hello {}, we informed that our {} product called {} already sold out...",
